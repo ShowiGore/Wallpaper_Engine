7a4c25941180d8d8da3b2eea317f1e8b777ff27d
ShowiGore <david444488888888@gmail.com>
2021-07-05 23:22:42 +0200

    added randomness and proportionality

--- double_pendulum/script.js
+++ double_pendulum/script.js
@@ -1,36 +1,43 @@
 let canvas
-let ctx
+let ctx;
 
-let min_size
-let max_size
+let min_size;
+let max_size;
 
 const g = 9.80665;
-const time = 0.2;
+const time = 0.1;
 let x0;
 let y0;
 
-let l1 = 100;
-let l2 = 100;
-let m1 = 0.1;
-let m2 = 0.1;
-let theta1 = 1 * (Math.PI) / 2;
-let theta2 = 3 * (Math.PI) / 2;
-let r0 = 20;
-let r1 = 20;
-let r2 = 20;
+let l_min = 50;
+let l_max = 100;
+let m_min = 0.1;
+let m_max = 0.4;
+let theta_min = 0.5 * (Math.PI);
+let theta_max = 1.5 * (Math.PI);
 
+let l1 = Math.random() * (l_max - l_min) + l_min;
+let l2 = Math.random() * (l_max - l_min) + l_min;
+let m1 = Math.random() * (m_max - m_min) + m_min;
+let m2 = Math.random() * (m_max - m_min) + m_min;
+let theta1 = Math.random() * (theta_max - theta_min) + theta_min;
+let theta2 = Math.random() * (theta_max - theta_min) + theta_min;
 
+let r0;
+let r1;
+let r2;
+let s1;
+let s2;
+let x1
+let y1
+let x2
+let y2
 
 let mu = 1 + m1 / m2;
 let theta1_d2 = 0;
 let theta2_d2 = 0;
 let theta1_d = 0;
 let theta2_d = 0;
-
-let x1
-let y1
-let x2
-let y2
 
 window.wallpaperPropertyListener = {
   applyUserProperties: function(properties) {
@@ -57,17 +64,27 @@ window.wallpaperPropertyListener = {
 }
 
 function run() {
-  theta1_d2 = (g * (Math.sin(theta2) * Math.cos(theta1 - theta2) - mu * Math.sin(theta1)) - (l2 * theta2_d * theta2_d + l1 * theta1_d * theta1_d * Math.cos(theta1 - theta2)) * Math.sin(theta1 - theta2)) / (l1 * (mu - Math.cos(theta1 - theta2) * Math.cos(theta1 - theta2)));
-  theta2_d2 = (mu * g * (Math.sin(theta1) * Math.cos(theta1 - theta2) - Math.sin(theta2)) + (mu * l1 * theta1_d * theta1_d + l2 * theta2_d * theta2_d * Math.cos(theta1 - theta2)) * Math.sin(theta1 - theta2)) / (l2 * (mu - Math.cos(theta1 - theta2) * Math.cos(theta1 - theta2)));
+  let st1 = Math.sin(theta1);
+  let st2 = Math.sin(theta2);
+  let ct1Mt2 = Math.cos(theta1 - theta2)
+  let st1Mt2 = Math.sin(theta1 - theta2);
+  let mMct1mt2Mct1mt2 = mu - ct1Mt2 * ct1Mt2;
+  let t1_ds = theta1_d * theta1_d;
+  let t2_ds = theta2_d * theta2_d;
+  let l1Mt1_ds = l1 * t1_ds;
+  let l2Mt2_ds = l2 * t2_ds;
+
+  theta1_d2 = (g * (st2 * ct1Mt2 - mu * st1) - (l2Mt2_ds + l1Mt1_ds * ct1Mt2) * st1Mt2) / (l1 * mMct1mt2Mct1mt2);
+  theta2_d2 = (mu * g * (st1 * ct1Mt2 - st2) + (mu * l1Mt1_ds + l2Mt2_ds * ct1Mt2) * st1Mt2) / (l2 * mMct1mt2Mct1mt2);
   theta1_d += theta1_d2 * time;
   theta2_d += theta2_d2 * time;
   theta1 += theta1_d * time;
   theta2 += theta2_d * time;
 
-  x1 = x0 + l1 * Math.sin(theta1);
-  y1 = y0 + l1 * Math.cos(theta1);
-  x2 = x0 + l1 * Math.sin(theta1) + l2 * Math.sin(theta2);
-  y2 = y0 + l1 * Math.cos(theta1) + l2 * Math.cos(theta2);
+  x1 = x0 + s1 * Math.sin(theta1);
+  y1 = y0 + s1 * Math.cos(theta1);
+  x2 = x0 + s1 * Math.sin(theta1) + s2 * Math.sin(theta2);
+  y2 = y0 + s1 * Math.cos(theta1) + s2 * Math.cos(theta2);
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -90,27 +107,48 @@ function circle(x, y, r) {
 function line(x1, y1, x2, y2) {
   ctx.beginPath();
   ctx.strokeStyle = "#FFFFFF";
-  ctx.lineWidth = 8;
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.stroke();
 }
 
-//canvas load
-window.onload = function() {
+function setup() {
   canvas = document.querySelector("#canvas");
   ctx = canvas.getContext("2d");
   canvas.width = canvas.scrollWidth;
   canvas.height = canvas.scrollHeight;
-  x0 = canvas.width/2;
-  y0 = canvas.height/2;
+  min_size = Math.min(canvas.width, canvas.height);
+  //max_size = Math.max(canvas.width, canvas.height);
+
+  ctx.lineWidth = min_size / 60;
+
+  r_min = min_size / 40;
+  r_max = min_size / 20;
+  s_min = min_size * 1 / 6 - r_max / 2;
+  s_max = min_size * 1 / 4 - r_max / 2;
+
+  a = (r_min - r_max) / (m_min - m_max);
+  b = (((-m_max) * r_min + m_min * r_max) / (m_min - m_max));
+  r0 = (r_min + r_max) / 2;
+  r1 = a * m1 + b;
+  r2 = a * m2 + b;
+
+  a = (s_min - s_max) / (l_min - l_max);
+  b = (((-l_max) * s_min + l_min * s_max) / (l_min - l_max));
+  s1 = a * l1 + b;
+  s2 = a * l2 + b;
+
+  x0 = canvas.width / 2;
+  y0 = canvas.height / 2;
+}
+
+//canvas load
+window.onload = function() {
+  setup();
   window.requestAnimationFrame(run);
 }
 
 //canvas resize
 window.onresize = function() {
-  canvas.width = canvas.scrollWidth;
-  canvas.height = canvas.scrollHeight;
-  x0 = canvas.width/2;
-  y0 = canvas.height/2;
+  setup();
 }
